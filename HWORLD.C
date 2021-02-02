e9384a8e5677a36d2d68b42f90f086f9406b6f86
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-02-02 18:10:18 +0530

    Add files via upload

---
+++ HWORLD.C
@@ -0,0 +1,20 @@
+//Pre-Processor Directives
+#include<stdio.h>
+#include<conio.h>
+
+/*
+	#--> Pre-Processor
+	include
+	stdio.h - header file/directory
+
+	stdio: standard input / output
+
+	conio: Console/output screen input / output
+*/
+
+void main()  //main(): function    // entry point
+{
+	clrscr(); // clear screen
+	printf("Hello World");
+	getch(); // get character
+}
